23957de5dabf30876d78507ae839a4e0d862ed68
Debora Bacelar <bacelar@sunnit.com.br>
2020-12-08 17:28:42 -0300

    quase ok

--- src/app/dashboard/create-class/create-class.component.ts
+++ src/app/dashboard/create-class/create-class.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-create-class',
   templateUrl: './create-class.component.html',
-  styleUrls: ['./create-class.component.css']
+  styleUrls: ['./create-class.component.css'],
 })
 export class CreateClassComponent implements OnInit {
+  public questions: any[] = [
+    {
+      question: '',
+      answare1: '',
+      answare2: '',
+      answare3: '',
+      answare4: '',
+      rightAnsware: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
